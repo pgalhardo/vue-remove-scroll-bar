5eacf57a710849f7f328084c833f58ef052fa800
Pedro Galhardo <pgalhardo@icloud.com>
2025-02-12 22:21:51 +0000

    chore(deps): remove @chromatic-com/storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,6 @@ const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
   ],
   core: {
